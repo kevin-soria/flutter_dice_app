944a8aee2c9165fbedc7fcd44adc2e60a866184b
Kevin Soria <60301136+kevin-soria@users.noreply.github.com>
2020-06-26 13:53:11 -0400

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Snkr Trades'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
