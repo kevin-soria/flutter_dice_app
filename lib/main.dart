c5e1bb400bba4d33e6cf34a16e56a92e7b9562c0
Kevin Soria <60301136+kevin-soria@users.noreply.github.com>
2020-07-03 11:15:30 -0400

    try4 with set

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,14 @@ class ShoePage extends StatefulWidget {
 }
 
 class _ShoePageState extends State<ShoePage> {
-  List<String> urlStrings = [
+  Set<String> urlStrings = Set.from([
     'air-jordan-nike-air-max-shoe-sneakers-nike-png-clip-art.png',
     'images/unnamed.png',
     'Sneaker-PNG-Image.png',
     '16-165473_air-jordan-iv-travis-scott-hd-png-download.png',
     '28-289524_yeezy-350-v3-zebra-hd-png-download.png',
     '65-652876_sneakers-hd-png-download.png',
-  ];
+  ]);
   String shoeRoute =
       'air-jordan-nike-air-max-shoe-sneakers-nike-png-clip-art.png';
   @override
@@ -42,8 +42,8 @@ class _ShoePageState extends State<ShoePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  shoeRoute = urlStrings[3];
-//                  print(urlStrings[2]);
+//                  shoeRoute = urlStrings[3];
+                  print(urlStrings);
                 });
               },
 //              need to add shoe route button change to any of the asset pic in images on pressed
